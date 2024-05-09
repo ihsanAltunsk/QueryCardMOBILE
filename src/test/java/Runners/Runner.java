608b3b55aa6c6d162a8b1a56ac6c8946ab20bd39
ihsanAltunsk <altnskihsan@gmail.com>
2024-05-09 11:45:31 +0300

    maintenance

--- src/test/java/Runners/Runner.java
+++ src/test/java/Runners/Runner.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"},
         features = "src/test/resources/features",
         glue = {"stepdefinitions" , "hooks"},
+
         tags = "@smoke" ,
         dryRun = false
 )
