e08394f3f7709f5a1ce898a0191d9ad81345206e
ihsanAltunsk <altnskihsan@gmail.com>
2024-05-10 15:39:59 +0300

    maintenance

--- src/test/java/Runners/OuterRunner.java
+++ src/test/java/Runners/OuterRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 public class OuterRunner {
 
-    final String tags = "@EndToEnd";
+    final String tags = "@US14-TC01";
 
     @CucumberOptions(
             plugin = {
