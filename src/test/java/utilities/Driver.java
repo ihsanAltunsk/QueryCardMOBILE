599e664ff705b8665ae8b155fdf872afa2061b41
ihsanAltunsk <altnskihsan@gmail.com>
2024-05-10 13:26:22 +0300

    maintenance

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -8,22 +8,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-
 public class Driver extends Base {
     private Driver() {
     }
 
     public static AppiumDriver getAppiumDriver() {
-
-
-        /**  Gercek cihaz icin url "http:0.0.0.0:4723/wd/hub";
-         Emilator cihaz icin url "http:127.0.0.1:4723/wd/hub";
-         */
-
-
-        /**
-         * Driver null olduğunda telefonumuza ait özellikleri hazırlarız
-         */
         if (driver == null) {
             switch (ConfigReader.getProperty("platformName")) {
                 case "Android":
@@ -63,23 +52,6 @@ public class Driver extends Base {
                     throw new RuntimeException("Desteklenmeyen Platform");
             }
         }
-
-        /**
-         * Her test basinda uygulamanin sifirlanmasini ve en bastan olmasini saglar.
-         * NoReset=true olursa uygulama kaldigi noktadan devam eder
-         */
-
-     /*   if (ConfigReader.getProperty("platformName").equals("Android")) {
-            assert appiumServerUrl != null;
-            try {
-                driver = new AppiumDriver(new URL("http://127.0.0.1:4723"), options);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
-            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        } else {
-            throw new UnsupportedOperationException(" Invalid Platform Name ");
-        }*/
         return driver;
     }
 }
